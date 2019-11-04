e27e000596c12cf85c2155abb5831e0293200940
Jaru Sahassakunakorn <jaru_sah@truecorp.co.th>
2019-11-04 17:00:19 +0700

    Updates

---
+++ precache-manifest.e832b2fe6fa29f0e9b0fdb15960777f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a09bb260bb24deae35d4d572225a1d2",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "dad0795d7aab583a7daf",
+    "url": "/react/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "f44ec3f62f7c05e9ffd9",
+    "url": "/react/static/js/2.8ca14d58.chunk.js"
+  },
+  {
+    "revision": "dad0795d7aab583a7daf",
+    "url": "/react/static/js/main.63d64b46.chunk.js"
+  },
+  {
+    "revision": "805add0941ca609cba57",
+    "url": "/react/static/js/runtime-main.9257aae5.js"
+  }
+]);
